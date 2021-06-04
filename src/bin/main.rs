8102b365acbbc7321a61d97790837a231ba1aeb9
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-06-03 21:23:15 -0700

    added one command to get something started

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,23 @@
 use findiplib::hello_world;
 
+use clap::{App, Arg};
+
 pub fn main() {
+    let matches = App::new("findip")
+        .version("1.0.0")
+        .author("Yash Kulshrestha (@YashdalfTheGray)")
+        .about("A lightweight utility that finds out and reports public IP addresses")
+        .arg(
+            Arg::with_name("notifier")
+                .short("n")
+                .long("notifier")
+                .value_name("NOTIFIER")
+                .help("The notifier strategy to use")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    println!("{:?}", matches);
+
     hello_world(Option::None);
 }
