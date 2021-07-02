53993f43289561a607cbcb813db0e0e294be3ab3
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-07-02 10:16:01 -0400

    added a properties type for the rest api notifier

--- src/properties.rs
+++ src/properties.rs
@@ -1,14 +1,20 @@
-use std::path::Path;
+use std::{iter::Map, path::Path};
 
-#[derive(PartialEq, Eq, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct FileProperties {
     overwrite: bool,
     file_path: Box<Path>,
 }
 
-#[derive(PartialEq, Eq, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct S3Properties {
     assume_role_arn: String,
     region: String,
     bucket_name: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct RestProperties {
+    endpoint: url::Url,
+    headers: Map<String, String>,
 }
