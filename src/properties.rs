ea20387f5c15c6c66490ebdb1c628100885077db
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-07-02 10:12:16 -0400

    added an s3 properties struct as well

--- src/properties.rs
+++ src/properties.rs
@@ -1,5 +1,14 @@
+use std::path::Path;
+
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct FileProperties {
     overwrite: bool,
-    file_path: String,
+    file_path: Box<Path>,
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub struct S3Properties {
+    assume_role_arn: String,
+    region: String,
+    bucket_name: String,
 }
