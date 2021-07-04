dc8d35ec8aebc52a9ef31e49b946f86713510d02
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-07-03 20:43:09 -0700

    added serialize and deserialize to the properties

--- src/properties.rs
+++ src/properties.rs
@@ -1,20 +1,21 @@
-use std::{iter::Map, path::Path};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, path::Path};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct FileProperties {
     overwrite: bool,
     file_path: Box<Path>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct S3Properties {
     assume_role_arn: String,
     region: String,
     bucket_name: String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RestProperties {
     endpoint: url::Url,
-    headers: Map<String, String>,
+    headers: HashMap<String, String>,
 }
