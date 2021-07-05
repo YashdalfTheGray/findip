dbf54118e406fe42a2801f098ec88c4456bdb3c0
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-07-05 11:26:07 -0700

    fixed a compiler warning

--- src/properties.rs
+++ src/properties.rs
@@ -1,23 +1,23 @@
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, path::Path};
 
-#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
 pub struct FileProperties {
     overwrite: bool,
     file_path: Box<Path>,
 }
 
-#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
 pub struct S3Properties {
     assume_role_arn: String,
     region: String,
     bucket_name: String,
 }
 
-#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
 pub struct RestProperties {
     endpoint: url::Url,
     #[serde(default)]
