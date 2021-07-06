fd78754d3b11ece9b2484625b88b011fe4039021
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-07-06 04:55:28 -0700

    we don't really need serialize, just deserialize, really unmarshal

--- src/properties.rs
+++ src/properties.rs
@@ -1,31 +1,31 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use std::{collections::HashMap, path::Path};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all(deserialize = "snake_case"))]
 pub struct FileProperties {
     overwrite: bool,
     file_path: Box<Path>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all(deserialize = "snake_case"))]
 pub struct S3Properties {
     assume_role_arn: String,
     region: String,
     bucket_name: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all(deserialize = "snake_case"))]
 pub struct RestProperties {
     endpoint: url::Url,
     #[serde(default)]
     headers: HashMap<String, String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all(serialize = "camelCase", deserialize = "snake_case"))]
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all(deserialize = "snake_case"))]
 pub struct ConfigFile<T> {
     cron: String,
     notify_on_change_only: bool,
