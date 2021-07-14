9f290a3508037bbd2516b6594350cc8d6bdc97f9
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-07-14 02:49:45 -0700

    split up the thing that finds the ip and the thing that schedules it

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,11 @@ pub trait IPNotifier {
     fn notify_on_change();
 }
 
-pub fn find_external_ip(_notify_method: NotifierStrategy) -> std::net::IpAddr {
+pub fn find_external_ip(_config: properties::ConfigFile) -> std::net::IpAddr {
     return IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+}
+
+pub fn schedule_ip_notification(config: properties::ConfigFile) {
+    println!("{:?}", config);
+    println!("{:?}", find_external_ip(config))
 }
