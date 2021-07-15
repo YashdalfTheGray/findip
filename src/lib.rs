5283595f8aa17097dcfb6b719a81b6de7bd59dc0
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-07-15 04:38:04 -0700

    moved the notifier trait to another file because there is more there

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
 use std::net::{IpAddr, Ipv4Addr};
 
+pub mod notifier;
 pub mod properties;
-
-pub trait IPNotifier {
-    fn notify();
-    fn notify_on_change();
-}
 
 pub fn find_external_ip(_config: properties::ConfigFile) -> std::net::IpAddr {
     return IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
---
+++ src/notifier.rs
@@ -0,0 +1,4 @@
+pub trait IPNotifier {
+    fn notify();
+    fn notify_on_change();
+}
