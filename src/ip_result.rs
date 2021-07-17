ed8877979c04884b3ec42f2df320856dcf42eec4
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-07-17 02:28:28 -0700

    added an ip error struct

--- src/ip_result.rs
+++ src/ip_result.rs
@@ -1,10 +1,27 @@
-use std::{net::IpAddr, sync::Mutex};
+use std::{error::Error, fmt, net::IpAddr, sync::Mutex};
 
 use chrono::{DateTime, Utc};
 
 struct IPResult {
     pub ip: IpAddr,
     pub checked_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone)]
+pub struct IPError {}
+
+impl fmt::Display for IPError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "IP Address not found.",)
+    }
+}
+
+impl Error for IPError {}
+
+impl IPError {
+    pub fn new() -> Self {
+        IPError {}
+    }
 }
 
 pub struct IPResults {
