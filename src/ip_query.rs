3a54d069d3ac609e7342c0374a1861e0e0d5ac5d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-07-29 23:18:17 -0400

    added a simple get all ips and filter out empty implementation

--- src/ip_query.rs
+++ src/ip_query.rs
@@ -6,6 +6,16 @@ pub struct IpQueryParams {
     pub services: Vec<String>,
 }
 
-pub fn run_ip_query(_params: IpQueryParams) -> Result<IpAddr, IpError> {
+pub fn run_ip_query(params: IpQueryParams) -> Result<IpAddr, IpError> {
+    let _ips: Vec<String> = params
+        .services
+        .iter()
+        .map(|service| match reqwest::blocking::get(service) {
+            Ok(response) => response.text().unwrap_or("".to_string()),
+            Err(_) => "".to_string(),
+        })
+        .filter(|maybe_ip| !maybe_ip.is_empty())
+        .collect();
+
     Ok(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)))
 }
