4c8a3c6dca7beced523a62a1d65dcbc93af43b95
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-08-02 21:42:01 -0400

    added a file notifier struct

--- src/notifier.rs
+++ src/notifier.rs
@@ -1,8 +1,13 @@
-use std::net::IpAddr;
+use std::{fs::File, net::IpAddr};
 
 use crate::errors::IpError;
 
 pub trait IpNotifier {
     fn notify_success(&self, ip: IpAddr);
     fn notify_error(&self, err: IpError);
+}
+
+pub struct FileNotifier {
+    overwrite: bool,
+    file: Option<File>,
 }
