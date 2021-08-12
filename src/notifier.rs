53020472037cdea0794dea0e1fe7c322d3029a61
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-08-11 21:53:51 -0700

    started implementing notifier for filenotifier

--- src/notifier.rs
+++ src/notifier.rs
@@ -21,6 +21,35 @@ impl FileNotifier {
     }
 }
 
+impl IpNotifier for FileNotifier {
+    fn notify_success(&self, ip: IpAddr) {
+        if self.overwrite {
+            match fs::write(ip.to_string(), self.file_path.clone()) {
+                Ok(()) => (),
+                Err(_) => IpNotifier::notify_error(
+                    self,
+                    IpError::new(ErrorReason::FileWriteFailed(self.file_path.clone())),
+                ),
+            }
+        } else {
+            let mut file = fs::OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(self.file_path.clone())
+                .expect("failed to open file");
+
+            match file.write(&ip.to_string().into_bytes()) {
+                Ok(_) => (),
+                Err(_) => IpNotifier::notify_error(
+                    self,
+                    IpError::new(ErrorReason::FileWriteFailed(self.file_path.clone())),
+                ),
+            }
+        }
+    }
+    fn notify_error(&self, err: IpError) {}
+}
+
 pub struct S3Notifier {
     assume_role_arn: String,
     region: String,
